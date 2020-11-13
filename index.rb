a1d968fd9fe959fb7e969e52c26cf43ab4cdc71a
magas4891 <magasishe@gmail.com>
2020-11-13 17:03:24 +0200

    rules separated file

--- index.rb
+++ index.rb
@@ -1,19 +1,9 @@
 require_relative 'rule'
 require_relative 'checkout'
 require_relative 'item_list'
+require_relative 'rules'
 
-rule1 = {
-    item: 'A',
-    quantity: 3,
-    total_price: 75
-}
-rule2 = {
-    basket_total: 150,
-    discount: 20
-}
-rules = [rule1, rule2]
-
-co = Checkout.new(rules)
+co = Checkout.new(@rules)
 co.scan(@item_list[:A])
 co.scan(@item_list[:B])
 co.scan(@item_list[:A])
---
+++ rules.rb
@@ -0,0 +1,10 @@
+rule1 = {
+    item: 'A',
+    quantity: 3,
+    total_price: 75
+}
+rule2 = {
+    basket_total: 150,
+    discount: 20
+}
+@rules = [rule1, rule2]
