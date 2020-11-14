0355429d5b439a3a792ff1a4f6308d5f83e52e5b
magas4891 <magasishe@gmail.com>
2020-11-14 18:00:16 +0200

    moved to lib

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative '../checkout'
-require_relative '../rule'
+require_relative '../lib/checkout'
+require_relative '../lib/rule'
 
 RSpec.describe Checkout do
   subject { described_class.new(rules) }
